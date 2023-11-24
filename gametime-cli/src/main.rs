3fb513869c48a0fee522ead9d2372d840bf77887
Kathryn Long <squeeself@gmail.com>
2023-11-23 19:08:04 -0500

    stub cli args

--- gametime-cli/src/main.rs
+++ gametime-cli/src/main.rs
@@ -1,3 +1,44 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+/// Command line args
+#[derive(Debug, Parser)]
+#[command(version, about, long_about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+// Cli commands
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Package a gametime ROM
+    #[command(
+        short_flag = 'p',
+        long_flag = "package",
+        visible_alias = "build",
+        short_flag_alias = 'b',
+        long_flag_alias = "build"
+    )]
+    Package {
+        /// Package manifest file override
+        #[arg(value_name = "FILE")]
+        manifest: Option<PathBuf>,
+
+        /// Package output file
+        #[arg(short, long, value_name = "FILE")]
+        output: Option<PathBuf>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Command::Package {
+            manifest: _,
+            output: _,
+        } => {}
+    }
 }
